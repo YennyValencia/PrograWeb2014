44238c94c659411726ac6e71a6801a15232f21d5
YeseniaME <yesenia-me@hotmail.com>
2014-03-17 20:44:58 -0600

    Practcas al  17Mar

---
+++ Tareas/11130587/PRACTICAS/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(
+	function() {
+		/*
+		$('img').hide(1000);
+		*/
+		$('img').click(
+			function(){
+				$(this).hide(1000);
+
+			}
+
+
+			)
+		$('#btn1').click(
+			function(){
+				$('img').show(1000);
+			}
+			)
+
+}
+	);
